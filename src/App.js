5ca2fd7598efdb4e1acf9c10302ea0139b69523e
antonicasasayasg <antoni.casasayas1@gmail.com>
2021-10-09 12:45:24 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -26,29 +26,35 @@ const cardsData = [
 function App() {
   return (
     <div className="App">
-      <h2 className="text-center font-bold ml-4 text-2xl">TC</h2>
+      <h2 className="text-center font-bold ml-4 lg:text-4xl text-2xl">TC</h2>
       <div className="bg-gray-700 h-screen px-5 flex lg:items-center justify-between mb-10 sm:flex-col">
         <div className="flex flex-col text-center items-center h-5/6 justify-between ">
-          <h1 className="text-2xl mt-10 ">
+          <h1 className="text-2xl lg:mt-20 lg:text-4xl mt-10 ">
             We design e-commerce websites for fashion businesses.
           </h1>
-          <p>Grow your online business and increase your sales</p>
+          <p className="lg:text-lg lg:mt-4">
+            Grow your online business and increase your sales
+          </p>
           <YoutubeEmbed embedId="rzlSsng7ZdE" />
-          <button className="text-base">Learn more</button>
+          <button className="text-base mt-8">Learn more</button>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col lg:flex-row items-center justify-center">
         <span>Sleek design. </span>
         <span className="bold">Easier usability.</span>
         <span className="italics">Better performance.</span>
         <span className="bold">More sells.</span>
-        <button className="second text-base">Learn more</button>
+        <button className="second lg:hidden text-base">Learn more</button>
       </div>
+      <div className="hidden lg:flex justify-center">
+        <button className="second  text-base">Learn more</button>
+      </div>
+
       <div className="section-2">
-        <h3 className="text-2xl">Here's what you'll get...</h3>
-        <p className="text-lg">from our best-in-class web service</p>
+        <h3 className="text-2xl lg:text-4xl ">Here's what you'll get...</h3>
+        <p className="text-lg lg:text-xl">from our best-in-class web service</p>
       </div>
-      <div className="flex lg:mx-20 items-center flex-col justify-between">
+      <div className="flex lg:mx-20 items-center lg:flex-row flex-col justify-between">
         {cardsData.map((cardData) => (
           <Card
             img={cardData.img}
@@ -57,37 +63,43 @@ function App() {
           />
         ))}
       </div>
-      <div className="bg-gray-700 h-96 py-10 my-10 text-white flex justify-between items-center text-sm flex-col">
+      <div className="bg-gray-700 h-auto py-10 my-10 text-white flex justify-between items-center text-sm flex-col">
         <div className="flex flex-col items-center">
-          <span className="text-2xl font-bold  ">Working with us</span>
-          <span className=" text-base font-bold mt-4 ">ONBOARDING</span>
-          <span>Initial call to get assets, access and establish goals.</span>
+          <span className="text-2xl font-bold lg:text-4xl lg:mb-4 ">
+            Working with us
+          </span>
+          <span className=" text-base font-bold  mt-8 lg:mt-4 lg:text-xl ">
+            ONBOARDING
+          </span>
+          <span className="lg:text-base">
+            Initial call to get assets, access and establish goals.
+          </span>
         </div>
-        <div className="flex flex-col text-center items-center">
-          <span className="text-base font-bold mt-4 ">FLOW</span>
-          <span>
+        <div className="flex flex-col text-center my-10 lg:my-4 items-center">
+          <span className="text-base font-bold mt-4 lg:text-xl">FLOW</span>
+          <span className="lg:text-base">
             A fool-proof system for seamless workflow and consistent
             communication, so that we all stay on the same page.
           </span>
         </div>
-        <div className="flex flex-col text-center items-center">
-          <span className="text-base font-bold mt-4 ">REPORTING</span>
-          <span>
+        <div className="flex flex-col text-center lg:my-4 items-center">
+          <span className="text-base font-bold mt-4 lg:text-xl">REPORTING</span>
+          <span className="lg:text-base">
             You get a detailed report every week so you can see the results.
           </span>
         </div>
       </div>
 
       <div className="flex flex-col  text-center mt-20">
-        <span className="bold  text-2xl">Guaranteed Results</span>
-        <span>If you don't get amazing results we give your money back.</span>
+        <span className="bold lg:text-4xl text-2xl">Guaranteed Results</span>
+        <span className="lg:text-base">If you don't get amazing results we give your money back.</span>
       </div>
-      <div className="flex flex-col h-96 items-center mt-20">
-        <span className="text-2xl bold">Let's get started today!</span>
+      <div className="flex flex-col h-96 lg:h-screen items-center mt-20">
+        <span className="text-2xl lg:text-4xl bold">Let's get started today!</span>
         <span>Choose a suitable time and let's talk!</span>
 
         <div
-          className="calendly-inline-widget w-64 h-64 mt-4 border border-gray-300 rounded"
+          className="calendly-inline-widget w-64 h-64 mb-4 lg:w-3/6 lg:h-5/6 mt-4  rounded"
           data-url="https://calendly.com/antoni-casasayas"
         ></div>
       </div>
--- src/embed.js
+++ src/embed.js
@@ -1,14 +1,13 @@
 const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive ">
+  <div className="video-responsive w-full flex justify-center">
     <iframe
-      width="570"
-      height="320"
+      
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
-      className="w-full"
+      className="w-full lg:w-4/6 lg:h-96 h-full"
     />
   </div>
 );
